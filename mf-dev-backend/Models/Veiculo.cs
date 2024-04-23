eb37bc7150ee1ea86549151d4fd384d524ee10ed
Guilherme Radicchi <guilhermeradicchi@hotmail.com>
2024-04-23 09:26:26 -0300

    Configuração do DB e da Tabela Veiculo

---
+++ mf-dev-backend/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Obrigatório Inserir um Nome")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório Inserir a Placa")]
+        public string Placa { get; set; }
+        [Required(ErrorMessage = "Obrigatório Inserir o Ano de Fabricacao")]
+        public int AnoFabricacao { get; set; }
+        [Required(ErrorMessage = "Obrigatório Inserir o Ano do Modelo")]
+        public int AnoModelo { get; set; }
+    }
+}
